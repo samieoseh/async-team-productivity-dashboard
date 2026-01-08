053dc42d6712e0750ec89e359f90b902b33fdae1
Samie <samieoseh@gmail.com>
2026-01-08 09:18:13 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+
+export default function App() {
+  return (
+    <div>
+      <p className="text-red-500">App</p>
+      <Button variant="contained">Hello world</Button>;
+    </div>
+  );
+}

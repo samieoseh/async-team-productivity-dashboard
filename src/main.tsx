faebc43cae88404a29b1af5f1bc4ea11e82edfbd
Samie <samieoseh@gmail.com>
2026-01-12 09:30:50 +0100

    Add routing and authentication pages
    - Integrated react-router-dom for routing
    - Created SignInPage and SignUpPage components
    - Updated main.tsx to use RouterProvider and defined routes
    - Removed unused App component
    - Updated package.json and package-lock.json to include react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { createBrowserRouter, Outlet } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import SignInPage from "./app/auth/pages/SignInPage";
+import SignUpPage from "./app/auth/pages/SignUpPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/auth",
+    element: <Outlet />,
+    children: [
+      {
+        path: "sign-in",
+        element: <SignInPage />,
+        index: true,
+      },
+      {
+        path: "sign-up",
+        element: <SignUpPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
